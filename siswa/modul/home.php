421aa9d6afa9ddbad0005c6f81f427b728ad19bb
Muhfa31 <fazaputra977@gmail.com>
2022-11-11 13:47:24 +0700

    Commit

---
+++ siswa/modul/home.php
@@ -0,0 +1,77 @@
+<div class="panel-header bg-primary-gradient">
+					<div class="page-inner py-5">
+						<div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
+							<div>
+								<h2 class="text-white pb-2 fw-bold">Aplikasi Presensi</h2>
+								<h5 class="text-white op-7 mb-2">Selamat Datang, <b class="text-warning"><?=$data['nama_siswa']; ?></b></h5>
+							</div>
+						</div>
+					</div>
+				</div>
+<div class="page-inner mt--5">
+					<div class="row mt--2">
+						<div class="col-md-6">
+							<div class="card full-height">
+								<div class="card-body">
+										<div class="card-title">
+										<center>
+											<img src="../assets/img/logosekolah.png" width="100">
+											<br>
+											<b>SMA JAGAT RAYA</b>
+										</center>
+									</div>
+									<div class="card-category">
+										<center>
+											Jl. Bima Sakti - Desa Venus Kec. Bulan Kab. Bumi Kode Pos 67890
+											<br>Email : smajaya@gmail.com Telp.081222333444
+										</center>
+									</div>
+								
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="card">
+								<div class="card-body">
+									<ul class="nav nav-pills nav-secondary  nav-pills-no-bd nav-pills-icons justify-content-center" id="pills-tab-with-icon" role="tablist">
+										<li class="nav-item">
+											<a class="nav-link" href="?page=kehadiran">
+												<i class="fas fa-clipboard-list"></i>
+												Absensi
+											</a>
+										</li>
+										<li class="nav-item">
+											<a class="nav-link" id="pills-contact-tab-icon" data-toggle="pill" href="#pills-contact-icon" role="tab" aria-controls="pills-contact-icon" aria-selected="false">
+												<i class="fas fa-user-astronaut"></i>
+												About
+											</a>
+										</li>
+									</ul>
+									<div class="tab-content mt-2 mb-3" id="pills-with-icon-tabContent">
+										<div class="tab-pane fade" id="pills-home-icon" role="tabpanel" aria-labelledby="pills-home-tab-icon">
+											<hr>
+											<p>
+												
+											</p>
+
+										</div>
+										<div class="tab-pane fade" id="pills-profile-icon" role="tabpanel" aria-labelledby="pills-profile-tab-icon">
+
+											<p>
+										
+											</p>
+										</div>
+										<div class="tab-pane fade" id="pills-contact-icon" role="tabpanel" aria-labelledby="pills-contact-tab-icon">
+											<p>
+												<hr>
+
+												Aplikasi Absensi siswa ini dibuat untuk mendokumentasikan kehadiran siswa, Aplikasi sangat mudah digunakan (Berbasis web)
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+				</div>
